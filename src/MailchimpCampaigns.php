deeac749ac835639b06331561a9c88b229f4f2a2
ruscoe <danruscoe@gmail.com>
2016-03-29 12:18:27 -0700

    Added function to get a specific MailChimp campaign.

--- src/MailchimpCampaigns.php
+++ src/MailchimpCampaigns.php
@@ -18,4 +18,24 @@ class MailchimpCampaigns extends Mailchimp {
     return $this->request('GET', '/campaigns', NULL, $parameters);
   }
 
+  /**
+   * Gets information about a specific campaign.
+   *
+   * @param string $campaign_id
+   *   The ID of the campaign.
+   * @param array $parameters
+   *   Associative array of optional request parameters.
+   *
+   * @return object
+   *
+   * @see http://developer.mailchimp.com/documentation/mailchimp/reference/campaigns/#read-get_campaigns_campaign_id
+   */
+  public function getCampaign($campaign_id, $parameters = array()) {
+    $tokens = array(
+      'campaign_id' => $campaign_id,
+    );
+
+    return $this->request('GET', '/campaigns/{campaign_id}', $tokens, $parameters);
+  }
+
 }
