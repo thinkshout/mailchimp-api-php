c07f062477c0ef56edb0216a943795fe0b594328
ruscoe <danruscoe@gmail.com>
2016-04-04 15:10:13 -0700

    Added campaign report summary function to MailChimp campaigns.

--- src/MailchimpReports.php
+++ src/MailchimpReports.php
@@ -7,12 +7,35 @@ class MailchimpReports extends Mailchimp {
   /**
    * Gets a report summary for the authenticated account.
    *
+   * @param array $parameters
+   *   Associative array of optional request parameters.
+   *
    * @return object
    *
    * @see http://developer.mailchimp.com/documentation/mailchimp/reference/reports/#read-get_reports
    */
-  public function getSummary() {
-    return $this->request('GET', '/reports');
+  public function getSummary($parameters = array()) {
+    return $this->request('GET', '/reports', NULL, $parameters);
+  }
+
+  /**
+   * Gets a report summary for a specific campaign.
+   *
+   * @param string $campaign_id
+   *   The ID of the campaign.
+   * @param array $parameters
+   *   Associative array of optional request parameters.
+   *
+   * @return object
+   *
+   * @see http://developer.mailchimp.com/documentation/mailchimp/reference/reports/#read-get_reports_campaign_id
+   */
+  public function getCampaignSummary($campaign_id, $parameters = array()) {
+    $tokens = array(
+      'campaign_id' => $campaign_id,
+    );
+
+    return $this->request('GET', '/reports/{campaign_id}', $tokens, $parameters);
   }
 
 }
