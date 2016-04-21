b90c7c2ed8f74bc068ec529f6af2c22ccf2ba6d2
amyvs <amy.vaillancourtsals@thinkshout.com>
2016-04-21 16:24:28 -0700

    Clean up some tabbing/spacing issues.

--- src/MailchimpEcommerce.php
+++ src/MailchimpEcommerce.php
@@ -109,85 +109,86 @@ class MailchimpEcommerce extends Mailchimp {
    *
    * @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/#delete-delete_ecommerce_stores_store_id
    */
-     $tokens = array(
-       'store_id' => $store_id,
-     );
-
-     return $this->request('DELETE', '/ecommerce/stores/{store_id}', $tokens);
-   }
-
-   /**
-    * 	Get information about a store’s carts.
-    *
-    * @param array $store_id
-    *   The ID of the store where the carts exist.
-    * @param array $parameters
-    *   Associative array of optional request parameters.
-    *
-    * @return object
-    *
-    * @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/carts/#read-get_ecommerce_stores_store_id_carts
-    */
-   public function getCarts($store_id, $parameters = array()) {
-     $tokens = array(
-       'store_id' => $store_id,
-     );
-
-     return $this->request('GET', '/ecommerce/stores/{store_id}/carts', $tokens, $parameters);
-   }
-
-   /**
-    * Get information about a specific cart.
-    *
-    * @param string $store_id
-    *   The ID of the store.
-    * @param string $cart_id
-    *   The ID of the cart.
-    *
-    * @return object
-    *
-    * @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/carts/#read-get_ecommerce_stores_store_id_carts_cart_id
-    */
-    public function getCart($store_id, $cart_id, $parameters = array()) {
-      $tokens = array(
-        'store_id' => $store_id,
-        'cart_id' => $cart_id,
-      );
-
-      return $this->request('GET', '/ecommerce/stores/{store_id}/carts/{cart_id}', $tokens, $parameters);
-    }
-
-    /**
-     * Adds a new cart to a store.
-     *
-     * @param string $id
-     *  The unique identifier for the cart.
-     * @param object $customer
-     *  Information about a specific customer.
-     * @param string $currency_code
-     *  The three-letter ISO 4217 code for the currency that the cart uses.
-     * @param float $order_total
-     *  The order total for the cart.
-     * @param bool $batch
-     * TRUE to create a new pending batch operation.
-     *
-     * @return object
-     *
-     * @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/carts/#create-post_ecommerce_stores_store_id_carts
-     */
-     public function addCart($store_id, $id, $customer, $currency_code, $order_total, $parameters = array(), $batch = FALSE) {
-       $tokens = array(
-         'store_id' => $store_id,
-       );
-
-       $parameters += array(
-         'id' => $id,
-         'customer' => $customer,
-         'currency_code' => $currency_code,
-         'order_total' => $order_total,
-       );
-
-       return $this->request('POST', '/ecommerce/stores/{store_id}/carts', $tokens, $parameters, $batch);
-     }
   public function deleteStore($store_id) {
+   $tokens = array(
+     'store_id' => $store_id,
+   );
+
+   return $this->request('DELETE', '/ecommerce/stores/{store_id}', $tokens);
+  }
+
+  /**
+   * Get information about a store’s carts.
+   *
+   * @param array $store_id
+   *   The ID of the store where the carts exist.
+   * @param array $parameters
+   *   Associative array of optional request parameters.
+   *
+   * @return object
+   *
+   * @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/carts/#read-get_ecommerce_stores_store_id_carts
+   */
+  public function getCarts($store_id, $parameters = array()) {
+   $tokens = array(
+     'store_id' => $store_id,
+   );
+
+   return $this->request('GET', '/ecommerce/stores/{store_id}/carts', $tokens, $parameters);
+  }
+
+  /**
+   * Get information about a specific cart.
+   *
+   * @param string $store_id
+   *   The ID of the store.
+   * @param string $cart_id
+   *   The ID of the cart.
+   *
+   * @return object
+   *
+   * @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/carts/#read-get_ecommerce_stores_store_id_carts_cart_id
+   */
+  public function getCart($store_id, $cart_id, $parameters = array()) {
+    $tokens = array(
+      'store_id' => $store_id,
+      'cart_id' => $cart_id,
+    );
+
+    return $this->request('GET', '/ecommerce/stores/{store_id}/carts/{cart_id}', $tokens, $parameters);
+  }
+
+  /**
+   * Adds a new cart to a store.
+   *
+   * @param string $id
+   *  The unique identifier for the cart.
+   * @param object $customer
+   *  Information about a specific customer.
+   * @param string $currency_code
+   *  The three-letter ISO 4217 code for the currency that the cart uses.
+   * @param float $order_total
+   *  The order total for the cart.
+   * @param bool $batch
+   * TRUE to create a new pending batch operation.
+   *
+   * @return object
+   *
+   * @see http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/carts/#create-post_ecommerce_stores_store_id_carts
+   */
+  public function addCart($store_id, $id, $customer, $currency_code, $order_total, $parameters = array(), $batch = FALSE) {
+    $tokens = array(
+     'store_id' => $store_id,
+    );
+
+    $parameters += array(
+     'id' => $id,
+     'customer' => $customer,
+     'currency_code' => $currency_code,
+     'order_total' => $order_total,
+    );
+
+    return $this->request('POST', '/ecommerce/stores/{store_id}/carts', $tokens, $parameters, $batch);
+  }
+
 }
