ebb16e58e9f51ed75bcbdd369256bf010f15803f
ruscoe <danruscoe@gmail.com>
2016-07-21 16:06:15 -0700

    Implement eCommerce test functions to store data between tests.

--- tests/src/MailchimpEcommerce.php
+++ tests/src/MailchimpEcommerce.php
@@ -10,6 +10,20 @@ namespace Mailchimp\Tests;
 class MailchimpEcommerce extends \Mailchimp\MailchimpEcommerce {
 
   /**
+   * Storage for stores. Used in place of real MailChimp API.
+   *
+   * @var array $stores
+   */
+  private $stores = [];
+
+  /**
+   * Storage for customers. Used in place of real MailChimp API.
+   *
+   * @var array $customers
+   */
+  private $customers = [];
+
+  /**
    * @inheritdoc
    */
   public function __construct($api_key = 'apikey', $api_user = 'apikey', $timeout = 60) {
@@ -22,6 +36,55 @@ class MailchimpEcommerce extends \Mailchimp\MailchimpEcommerce {
 
   public function getEndpoint() {
     return $this->endpoint;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getStore($store_id, $parameters = []) {
+
+    if (!empty($this->stores)) {
+      var_dump('getStore');
+      die(var_dump($this->stores));
+    }
+
+    return (isset($this->stores[$store_id])) ? $this->stores[$store_id] : NULL;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function addStore($id, $store, $batch = FALSE) {
+    $parameters = [
+      'id' => $id,
+    ];
+    $parameters += $store;
+
+    $this->stores[$id] = $parameters;
+
+    var_dump('addStore: ' . $id);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getCustomer($store_id, $customer_id, $parameters = []) {
+    if (isset($this->customers[$store_id])) {
+      return (isset($this->customers[$store_id][$customer_id])) ? $this->customers[$store_id][$customer_id] : NULL;
+    }
+
+    return NULL;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function addCustomer($store_id, $customer, $batch = FALSE) {
+    if (!isset($this->customers[$store_id])) {
+      $this->customers[$store_id] = [];
+    }
+
+    $this->customers[$store_id][$customer['id']] = $customer;
   }
 
 }
