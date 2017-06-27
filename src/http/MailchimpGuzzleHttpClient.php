524abadbb0e25ace2f772c91c8fdbbf7c42a3a45
ruscoe <danruscoe@gmail.com>
2017-06-26 19:59:48 -0700

    Populate MailchimpGuzzleHttpClient. #56

--- src/http/MailchimpGuzzleHttpClient.php
+++ src/http/MailchimpGuzzleHttpClient.php
@@ -2,6 +2,10 @@
 
 namespace Mailchimp\http;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Mailchimp\MailchimpAPIException;
+
 /**
  * An HTTP client for use with the MailChimp API using Guzzle.
  *
@@ -10,10 +14,55 @@ namespace Mailchimp\http;
 class MailchimpGuzzleHttpClient implements MailchimpHttpClientInterface {
 
   /**
+   * The GuzzleHttp client.
+   *
+   * @var Client $client
+   */
+  private $guzzle;
+
+  /**
+   * MailchimpGuzzleHttpClient constructor.
+   *
+   * @param array $config
+   *   Guzzle HTTP configuration options.
+   *   Currently supports only 'timeout'.
+   */
+  public function __construct($config = []) {
+    $this->guzzle = new Client($config);
+  }
+
+  /**
    * @inheritdoc
    */
   public function handleRequest($method, $uri = '', $options = [], $parameters = [], $returnAssoc = FALSE) {
-    // TODO: Handle HTTP request via Guzzle.
+    if (!empty($parameters)) {
+      if ($method == 'GET') {
+        // Send parameters as query string parameters.
+        $options['query'] = $parameters;
+      }
+      else {
+        // Send parameters as JSON in request body.
+        $options['json'] = (object) $parameters;
+      }
+    }
+
+    try {
+      $response = $this->guzzle->request($method, $uri, $options);
+      $data = json_decode($response->getBody(), $returnAssoc);
+
+      return $data;
+    }
+    catch (RequestException $e) {
+      $response = $e->getResponse();
+      if (!empty($response)) {
+        $message = $e->getResponse()->getBody();
+      }
+      else {
+        $message = $e->getMessage();
+      }
+
+      throw new MailchimpAPIException($message, $e->getCode(), $e);
+    }
   }
 
 }
