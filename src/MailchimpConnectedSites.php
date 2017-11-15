8356e7f31312a9d50fabff140843ad34a241b757
ruscoe <danruscoe@gmail.com>
2017-11-14 21:22:43 -0800

    Add Connected Sites library.

---
+++ src/MailchimpConnectedSites.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mailchimp;
+
+/**
+ * Mailchimp Connected Sites library.
+ *
+ * @package Mailchimp
+ *
+ * @see https://kb.mailchimp.com/integrations/connected-sites/about-connected-sites
+ */
+class MailchimpConnectedSites extends Mailchimp {
+
+  /**
+   * Gets information about all connected sites for the authenticated account.
+   *
+   * @param array $parameters
+   *   Associative array of optional request parameters.
+   *
+   * @return object
+   */
+  public function getConnectedSites($parameters = []) {
+    return $this->request('GET', '/connected-sites', NULL, $parameters);
+  }
+
+}
