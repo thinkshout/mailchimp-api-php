ff644ee13a9690db1e84791c6352621a47a7bfa8
ruscoe <danruscoe@gmail.com>
2017-11-14 22:14:45 -0800

    Add functionality to retrieve a specific connected site.

--- src/MailchimpConnectedSites.php
+++ src/MailchimpConnectedSites.php
@@ -23,4 +23,22 @@ class MailchimpConnectedSites extends Mailchimp {
     return $this->request('GET', '/connected-sites', NULL, $parameters);
   }
 
+  /**
+   * Gets a connected site.
+   *
+   * @param string $connected_site_id
+   *   The ID of the connected site.
+   * @param array $parameters
+   *   Associative array of optional request parameters.
+   *
+   * @return object
+   */
+  public function getConnectedSite($connected_site_id, $parameters = []) {
+    $tokens = [
+      'connected_site_id' => $connected_site_id,
+    ];
+
+    return $this->request('GET', '/connected-sites/{connected_site_id}', $tokens, $parameters);
+  }
+
 }
